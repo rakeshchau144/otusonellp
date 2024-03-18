2c29cb109dd1fe717a822d48615fd41dbbf43630
rakeshoutsone <rakeshoutsone@gmail.com>
2024-03-18 16:12:42 +0530

    add

--- src/main/java/com/otusone/Backend/model/Chat.java
+++ src/main/java/com/otusone/Backend/model/Chat.java
@@ -1,9 +1,5 @@
 package com.otusone.Backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 @Data
 public class Chat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer chatId;
+
     private String fullName;
     private String email;
     private String number;
--- src/main/java/com/otusone/Backend/service/ChatService.java
+++ src/main/java/com/otusone/Backend/service/ChatService.java
@@ -1,9 +1,7 @@
 package com.otusone.Backend.service;
 
 import com.otusone.Backend.model.Chat;
-import com.otusone.Backend.repository.ChatRepo;
 import com.otusone.Backend.service.emailUtility.EmailHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,8 +10,7 @@ import javax.mail.MessagingException;
 
 @Service
 public class ChatService {
-    @Autowired
-    private ChatRepo chatRepo;
+
 
     public ResponseEntity<String> letsTalk(Chat chat) throws MessagingException {
         String message = "Mr. "+chat.getFullName()+" " +
@@ -23,11 +20,10 @@ public class ChatService {
                 "His details." +
                 "\n" +
                 "Name :- "+chat.getFullName()+"" +
-                "\n Email :- "+chat.getEmail()+"" +
-                "\n Phone Number :- "+chat.getNumber()+"" +
-                "\n Topic he want to talk - "+chat.getMassage()+"";
+                "\nEmail :- "+chat.getEmail()+"" +
+                "\nPhone Number :- "+chat.getNumber()+"" +
+                "\nTopic he want to talk - "+chat.getMassage()+"";
         EmailHandler.sendEmail("rakeshoutsone@gmail.com","Client want chat with you",message);
-        chatRepo.save(chat);
         return new ResponseEntity<>("message send to admin !!", HttpStatus.OK);
     }
 }
